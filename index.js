f8e83f2cd8a01c53158af46e17b97c495fe8b6f7
Lidkin <101173544+Lidkin@users.noreply.github.com>
2023-03-15 17:59:27 +0200

    1 file modified

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const settings = {
 const sketch = ({ context, width, height }) => {
   let x, y, w, h, fill, stroke, blend;
 
-  const num = 30;
-  const degrees = -40;
+  const num = 40;
+  const degrees = -45;
   const arrows = [];
   const arrowColors = [random.pick(risoColors), random.pick(risoColors)];
 
   const mask = {
-    radius: width * 0.55,
-    sides: 6,
+    radius: width * 0.45,
+    sides: 46,
     x: width * 0.5,
     y: height * 0.5,
   };
@@ -29,29 +29,28 @@ const sketch = ({ context, width, height }) => {
   const tinyStroke = random.pick(risoColors).hex;
   const arrowStroke = random.pick(risoColors).hex;
 
-  
-
   for (let i = 0; i < num; i++) {
     x = random.range(0, width);
     y = random.range(0, height);
-    w = random.range(300, 700);
-    h = random.range(50, 300);
+    w = random.range(300, 600);
+    h = random.range(100, 400);
     fill = random.pick(arrowColors).hex;
     stroke = random.pick(arrowColors).hex;
-   // blend = 'color-dodge';
-    blend = random.value() > 0.5 ? 'source-over' : 'hard-light'; //screen, multiply, color-burn, luminosity, overlay, hard-light
-
+    // blend = 'color-dodge';
+    blend = random.value() > 0.5 ? 'overlay' : 'source-over'; //screen, multiply, color-burn, luminosity, overlay, hard-light
 
     arrows.push({ x, y, w, h, fill, stroke, blend });
-  };
+  }
 
   return ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
 
     context.save();
-    context.translate( mask.x, mask.y );
+    context.translate(mask.x, mask.y);
+    //context.translate(mask.x * 1.97, mask.y * 0.89);
     drawPolygon({ context, radius: mask.radius, sides: mask.sides });
+    //drawSkewedArrow({ context, w: mask.x * 1.5, h: mask.y * 1.2, degrees: 135 });
 
     context.clip();
 
@@ -61,73 +60,79 @@ const sketch = ({ context, width, height }) => {
 
       context.save();
       context.translate(-mask.x, -mask.y);
+      //context.translate(-mask.x * 1.97, -mask.y * 0.89);
       context.translate(x, y);
 
       context.fillStyle = fill;
       context.strokeStyle = stroke;
       context.lineWidth = 15;
-      
+
       context.globalCompositeOperation = blend;
 
-      drawSkewedArrow({ context, w, h, degrees });
-/*
-      shadowColor = Color.offsetHSL(fill, 0, 0, -30);
+      shadowColor = Color.offsetHSL(fill, 0, 0, -10);
       shadowColor.rgba[3] = 0.5;
-
       context.shadowColor = Color.style(shadowColor.rgba);
-      context.shadowOffsetX = -20;
-      context.shadowOffsetY = 10;
-*/
-      context.fill();
 
-      context.shadowColor = null;
-      context.stroke();
+			context.shadowOffsetX = -10;
+			context.shadowOffsetY = 20;
 
-      context.globalCompositeOperation = 'multiply';
+      drawSkewedArrow({ context, w, h, degrees });
+  
+			context.fill();
+
+			context.shadowColor = null;
+			context.stroke();
 
       context.lineWidth = 3;
       context.strokeStyle = tinyStroke;
       context.stroke();
-
-      context.globalCompositeOperation = 'source-over';
-      drawTinyArrow({ context, w, h, degrees, arrowStroke });
+      
+      context.lineWidth = h * 0.04;
+      context.lineCap = 'round';
+      context.strokeStyle = Color.style(Color.offsetHSL(arrowStroke, 20, 15, 10));;
+      drawTinyArrow({ context, w, h, degrees });
+      context.stroke();
 
       context.restore();
     });
 
     context.restore();
 
-          //polygon outline
+    //polygon outline
 
-          context.save();
-          context.translate(mask.x, mask.y);
-          context.lineWidth = 40;
-      
-          drawPolygon({ context, radius: mask.radius - context.lineWidth, sides: mask.sides });  
-      
-          context.globalCompositeOperation = 'color-burn';
-          context.strokeStyle = arrowColors[0].hex;
-          shadowColor = Color.offsetHSL(fill, 0, 0, 20);
-          shadowColor.rgba[3] = 0.5;
-          context.shadowColor = Color.style(shadowColor.rgba);
+    context.save();
+    context.translate(mask.x, mask.y);
+    //context.translate(mask.x * 1.97, mask.y * 0.89);
+    context.lineWidth = 40;
 
-          context.stroke();
-          context.restore();
+    context.globalCompositeOperation = 'difference';
 
+    drawPolygon({
+      context,
+      radius: mask.radius - context.lineWidth * 0.5,
+      sides: mask.sides,
+    });
+    
+    //drawSkewedArrow({ context, w: mask.x * 1.5, h: mask.y * 1.2, degrees: 135 });
+    context.strokeStyle = Color.style(Color.offsetHSL(bgColor, 15, 10, 5));
+    shadowColor = Color.style(Color.offsetHSL(bgColor, 10, 15, -20));
+    context.shadowBlur = 20;
+    context.shadowColor = shadowColor;
+    context.stroke();
+    context.restore();
   };
 };
 
 const drawPolygon = ({ context, radius, sides }) => {
-  const slice = Math.PI * 2 / sides;
-  
+  const slice = (Math.PI * 2) / sides;
+
   context.beginPath();
   context.moveTo(0, -radius);
-  for(let i = 1; i < sides; i++) {
+  for (let i = 1; i < sides; i++) {
     const angle = i * slice - Math.PI * 0.5;
     context.lineTo(Math.cos(angle) * radius, Math.sin(angle) * radius);
   }
   context.closePath();
-
 };
 
 const drawSkewedArrow = ({ context, w, h, degrees }) => {
@@ -146,13 +151,10 @@ const drawSkewedArrow = ({ context, w, h, degrees }) => {
   context.restore();
 };
 
-const drawTinyArrow = ({ context, w, h, degrees, arrowStroke }) => {
+const drawTinyArrow = ({ context, w, h, degrees }) => {
   const angle = math.degToRad(degrees);
 
   context.save();
-  context.lineWidth = h * 0.04;
-  context.lineCap = 'round';
-  context.strokeStyle = arrowStroke;
   context.rotate(angle);
   context.beginPath();
   context.moveTo(w * 0.6, h * 0.7);
@@ -160,7 +162,6 @@ const drawTinyArrow = ({ context, w, h, degrees, arrowStroke }) => {
   context.lineTo(w * 0.6, h * 0.3);
   context.moveTo(0, h * 0.5);
   context.lineTo(w * 0.7, h * 0.5);
-  context.stroke();
   context.restore();
 };
 
